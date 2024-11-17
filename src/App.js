7d9afc8608cf6bf3073fe6d0daf1eaf55b3fdc60
Stephanie <syng828@gmail.com>
2024-11-17 11:33:34 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Create from './Create';
+import BlogDetails from './BlogDetail';
+import NotFound from './NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="content">
-        <h1>App Component</h1>
-        <Home/>
-      </div>
-    </div>
+    <Router>
+			<div className="App">
+				<Navbar></Navbar>
+          <div className="content">
+				    <Routes>
+					    <Route path='/' element={ <Home /> } />
+              <Route path ='/create' element = {<Create/>}/>
+              <Route path ='/blogs/:id'
+                element = {<BlogDetails/>}/>
+              <Route path = "*" element = {<NotFound/>}/>
+				    </Routes>
+        </div>
+			</div>
+		</Router>
   );
 }
 
